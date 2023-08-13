7dc3f5f1463f1200d8f35a38a166b648cb049d45
shefcodev <sherefjimoh@gmail.com>
2023-08-13 17:57:51 +0100

    destructured the props object

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+function ExpenseItem({ title, amount, date }) {
   return (
     <div className = "expense-item">
-      <div>{props.date.toDateString()}</div>
+      <div>{date.toDateString()}</div>
       <div className = "expense-item__description">
-        <h2>{props.title}</h2>
-        <div className = "expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className = "expense-item__price">${amount}</div>
       </div>
     </div>
   );
