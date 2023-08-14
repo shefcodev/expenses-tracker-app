e555d1b4556be2e5efc5bfc1a73b8df2acaa3cee
shefcodev <sherefjimoh@gmail.com>
2023-08-14 16:09:37 +0100

    added javascript logic to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,23 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem({ title, amount, date }) {
+function ExpenseItem({title, amount, date}) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+
   return (
-    <div className = "expense-item">
-      <div>{date.toDateString()}</div>
-      <div className = "expense-item__description">
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className = "expense-item__price">${amount}</div>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
-}
+} 
 
 export default ExpenseItem;
