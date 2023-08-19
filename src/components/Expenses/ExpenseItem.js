8ff2aae67cd74ddaac8b6d756601092e66691b79
shefcodev <sherefjimoh@gmail.com>
2023-08-19 23:06:01 +0100

    added react-state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [getTitle, setTitle] = useState(title);
+
+  const clickHandler = () => {
+    setTitle("No Expense");
+    console.log(getTitle);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{getTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick = {clickHandler}>Change Title</button>
     </Card>
   );
 }
